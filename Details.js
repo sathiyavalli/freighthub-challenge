4f38d955741436cd3e5a39072efb078193770d01
sathiyavalli <sathiyavalli1991@gmail.com>
2019-12-08 02:54:26 +0530

    Add files via upload

---
+++ Details.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import './App.css';
+import BootstrapTable from 'react-bootstrap-table-next';
+import '../node_modules/bootstrap-css-only/css/bootstrap.min.css';
+import filterFactory, { textFilter } from 'react-bootstrap-table2-filter';
+import paginationFactory from 'react-bootstrap-table2-paginator';
+import cellEditFactory from 'react-bootstrap-table2-editor';
+
+
+class Detail extends React.Component {
+  constructor(props) {
+      super(props);
+/*column declarations and intializing the data array*/
+ this.state = {
+    datas: [],
+    columns: [{
+      dataField: 'id',
+      text: 'Id',
+      sort: true
+    },
+    {
+      dataField: 'Item',
+      text: 'Item Name',
+      sort: true
+    }, {
+      dataField: 'Price',
+      text: 'Product Price',
+      sort: true
+    }]
+  }
+}
+/*retreiving the value from github json file*/
+ componentDidMount(){
+  let url = "https://my-json-server.typicode.com/sathiyavalli/frontend-frieghtChallenge/datas"
+  fetch(url)
+    .then(res => res.json())
+    .then((data) => {
+      this.setState({ datas: data })
+      console.log(this.state.datas)
+    })
+    .catch(console.log)
+  }
+ /*Table formation by adding all values that is retreived above*/
+render() {
+    return (
+      <div className="container">
+      <center><h3>Full Shipment Details</h3></center>
+        <BootstrapTable 
+        striped
+        hover
+        keyField='id' 
+        data={ this.state.datas } 
+        columns={ this.state.columns }/>
+      </div>
+    );
+  }
+}
+
+
+export default Detail;
+
+
+
